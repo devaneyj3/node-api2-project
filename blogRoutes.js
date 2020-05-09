fcaa538bef8a1c7536fedf9afca7bc2344612e5d
Jordan Devaney <jordandevaney@Jordans-MacBook-Air.local>
2020-05-09 16:36:09 -0400

    all edge cases show the right errors

--- blogRoutes.js
+++ blogRoutes.js
@@ -24,7 +24,6 @@ router.post('/', async (req, res) => {
         }
         else {
             const addPost = await db.insert(newPost);
-            console.log(addPost);
             res.status(201).send(addPost)
         }
     } catch {
@@ -53,7 +52,6 @@ router.put('/:id', async (req, res) => {
     const { id } = req.params;
     const changes = req.body;
     const postToUpdate = await db.update(id, changes) // why does it send back number
-    console.log(postToUpdate)
     try {
         if (req.body.title === '' || req.body.contents === '') {
             res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
@@ -68,19 +66,58 @@ router.put('/:id', async (req, res) => {
         res.status(500).json({ error: "The post information could not be modified." })
     }
 })
-router.get('/:id', (req, res) => {
+
+// GET post by ID
+router.get('/:id', async (req, res) => {
     const { id } = req.params;
-    res.status(200).send(`GET Post ${id} is working`)
+    let post = await db.findById(id)
+    try {
+        if (post.length === 0) {
+            res.status(404).json({ message: "The post with the specified ID does not exist." })
+            //this error message handler won't show up
+        }
+        return res.status(200).send(post)
+    } catch {
+        res.status(500).json({ error: "The post information could not be retrieved." })
+    }
+})
+// POST a new comment for specific ID
+router.post('/:id/comments', async (req, res) => {
+    const { id } = req.params;
+    const changes = req.body;
+    db.insertComment(changes)
+    let findPostByID = await db.findById(id)
+    
+    try {
+        if (findPostByID.length == 0) {
+            res.status(404).json({ message: "The post with the specified ID does not exist." })
+        }
+        //error for must have text
+        else if (req.body.text === '') {
+            res.status(400).json({ errorMessage: "Please provide text for the comment." });
+        } else {
+            res.status(200).send(changes) 
+        } 
+    } catch {
+        res.status(500).json({ error: "There was an error while saving the comment to the database" })
+    }
 })
 
-router.post('/:id/comments', (req, res) => {
+//GET comments of a specific post
+router.get('/:id/comments', async (req, res) => {
     const { id } = req.params;
-    res.status(200).send(`POST Comments for Post ${id} is working`)
-})
-
-router.get('/:id/comments', (req, res) => {
-    const { id } = req.params;
-    res.status(200).send(`GET Post Comments for ${id} is working`)
+    const findComments = await db.findPostComments(id);
+    try {
+        let post = await db.findById(id)
+        console.log(post)
+        if (post.length === 0) {
+            res.status(404).json({ message: "The post with the specified ID does not exist." })
+        } else {
+            res.status(200).send(findComments);
+        }
+    } catch {
+        res.status(500).json({ error: "The comments information could not be retrieved." })
+    }
 })
 
 module.exports = router;
