55532872c171052b16773d5cee631e9e442b4550
Jordan Devaney <jordandevaney@Jordans-MacBook-Air.local>
2020-05-08 23:14:41 -0400

    complete backend functionality to GET and POST posts

--- blogRoutes.js
+++ blogRoutes.js
@@ -2,12 +2,34 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.status(200).send('GET Posts is working')
+const db = require('./data/db');
+
+//get all posts
+
+router.get('/', async (req, res) => {
+    const answer = await db.find();
+    try {
+        res.status(200).send(answer)
+    } catch {
+        res.status(500).json({ error: "The posts information could not be retrieved." })
+    }
 });
 
-router.post('/', (req, res) => {
-    res.status(200).send('POST Posts is working')
+//post a new post to the database
+router.post('/', async (req, res) => {
+    const newPost = req.body;
+    try {
+        if (req.body.title === '' || req.body.contents === '') {
+            res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+        }
+        else {
+            const addPost = await db.insert(newPost);
+            console.log(addPost);
+            res.status(201).send(addPost)
+        }
+    } catch {
+        res.status(500).json({ error: "There was an error while saving the post to the database" })
+    }
 })
 
 router.delete('/:id', (req, res) => {
